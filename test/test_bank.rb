6dd2c4a5041cacd1837c5f25a8423db6f514c460
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-07-27 20:15:50 +1200

    Added tests for exchange rates and and parsing.

--- test/test_bank.rb
+++ test/test_bank.rb
@@ -1,13 +1,15 @@
 
 require 'helper'
 
-require 'latinum/bank'
+require 'latinum'
 require 'latinum/currencies/global'
 
 class BankTest < Test::Unit::TestCase
 	def setup
 		@bank = Latinum::Bank.new
 		@bank.import(Latinum::Currencies::Global)
+		
+		@bank << Latinum::ExchangeRate.new("NZD", "AUD", "0.5")
 	end
 	
 	def test_formatting
@@ -17,5 +19,23 @@ class BankTest < Test::Unit::TestCase
 		
 		resource = Latinum::Resource.new("391", "AUD")
 		assert_equal "$391.00 AUD", @bank.format(resource)
+		
+		resource = Latinum::Resource.new("-100", "NZD")
+		assert_equal "-$100.00 NZD", @bank.format(resource)
+	end
+	
+	def test_exchange
+		nzd = Latinum::Resource.new("10", "NZD")
+		
+		aud = @bank.exchange nzd, "AUD"
+		assert_equal Latinum::Resource.new("5", "AUD"), aud
+	end
+	
+	def test_parsing
+		assert_equal Latinum::Resource.new("5", "USD"), @bank.parse("$5")
+		assert_equal Latinum::Resource.new("5", "NZD"), @bank.parse("$5 NZD")
+		assert_equal Latinum::Resource.new("5", "EUR"), @bank.parse("€5")
+		
+		assert_equal Latinum::Resource.new("5", "NZD"), @bank.parse("5 NZD")
 	end
 end
